317d7f82e44b04f9a7ef0aea7037bb96b24bb795
Matthew Rister <matthewrister@outlook.com>
2024-05-21 22:31:35 -0700

    fix: validate depth attachment format support
    The depth buffer attachment is always created with the eD24UnormS8Uint
    format regardless of whether or not it's supported on the target
    platform which can result in an application crash. To fix this,
    candidate depth attachment formats are evaluated using physical device
    format properties to ensure that a format is supported prior to creating
    a depth attachment.

--- src/graphics/engine.cpp
+++ src/graphics/engine.cpp
@@ -31,6 +31,18 @@ vk::SampleCountFlagBits GetMsaaSampleCount(const vk::PhysicalDeviceLimits& physi
   return e1;
 }
 
+vk::Format GetDepthAttachmentFormat(const vk::PhysicalDevice physical_device) {
+  using enum vk::Format;
+  static constexpr std::array kTargetFormats{eD32Sfloat, eD32SfloatS8Uint, eD24UnormS8Uint, eD16Unorm, eD16UnormS8Uint};
+  for (const auto format : kTargetFormats) {
+    if (const auto format_properties = physical_device.getFormatProperties(format);
+        format_properties.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) {
+      return format;
+    }
+  }
+  throw std::runtime_error{"No supported depth attachment format"};
+}
+
 vk::UniqueRenderPass CreateRenderPass(const vk::Device device,
                                       const vk::SampleCountFlagBits msaa_sample_count,
                                       const vk::Format color_attachment_format,
@@ -180,7 +192,7 @@ Engine::Engine(const Window& window)
                                                 .usage = VMA_MEMORY_USAGE_AUTO,
                                                 .priority = 1.0f}},
       depth_attachment_{*device_,
-                        vk::Format::eD24UnormS8Uint,  // TODO(#54): check device support
+                        GetDepthAttachmentFormat(*device_.physical_device()),
                         swapchain_.image_extent(),
                         1,
                         msaa_sample_count_,
