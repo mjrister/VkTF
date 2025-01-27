33f88a6ef8e927a8828918e2763233bc640678da
Matthew Rister <matthewrister@outlook.com>
2025-01-26 20:17:16 -0800

    build: configure glslang optimization flags
    This commit passes SPIR-V optimization flags to glslang when compiling
    shaders as part of the project build. In release, the only option
    available is to optimize for size which is used. In debug mode, the
    optimizer is disabled and debug information is generated.

--- cmake/compile_shader.cmake
+++ cmake/compile_shader.cmake
@@ -1,5 +1,11 @@
 find_package(glslang CONFIG REQUIRED)
-get_target_property(GLSLANG_EXECUTABLE glslang::glslang-standalone LOCATION)
+get_target_property(GLSLANG_STANDALONE glslang::glslang-standalone LOCATION)
+
+if(CMAKE_BUILD_TYPE STREQUAL Debug)
+  set(GLSLANG_FLAGS -Od -g)
+else()
+  set(GLSLANG_FLAGS -Os)
+endif()
 
 function(compile_shader GLSL_FILENAME GLSL_STAGE)
   set(GLSL_SOURCE_FILEPATH ${SHADERS_SOURCE_DIR}/${GLSL_FILENAME})
@@ -7,8 +13,11 @@ function(compile_shader GLSL_FILENAME GLSL_STAGE)
   set(SPIRV_BINARY_FILEPATH ${GLSL_BINARY_FILEPATH}.spv)
 
   add_custom_command(OUTPUT  ${SPIRV_BINARY_FILEPATH} ${GLSL_BINARY_FILEPATH}
-                     COMMAND ${GLSLANG_EXECUTABLE} -S ${GLSL_STAGE} -V -o ${SPIRV_BINARY_FILEPATH} ${GLSL_SOURCE_FILEPATH}
-                     # create a symlink to the original source file to support runtime shader compilation
+                     COMMAND ${GLSLANG_STANDALONE} -S ${GLSL_STAGE}
+                                                   -V -o ${SPIRV_BINARY_FILEPATH}
+                                                   ${GLSLANG_FLAGS}
+                                                   ${GLSL_SOURCE_FILEPATH}
+                     # create a symlink to the original source file to allow runtime shader compilation
                      COMMAND ${CMAKE_COMMAND} -E create_symlink ${GLSL_SOURCE_FILEPATH} ${GLSL_BINARY_FILEPATH}
                      DEPENDS ${GLSL_SOURCE_FILEPATH}
                      COMMENT "Compiling ${GLSL_SOURCE_FILEPATH}")
