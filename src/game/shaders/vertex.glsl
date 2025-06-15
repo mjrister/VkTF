7d1c7adc436807abf9d68b283642f1794d9d772a
Matthew Rister <matthewrister@outlook.com>
2025-06-14 18:39:20 -0700

    wip: rework world-tangent recalculation

--- src/game/shaders/vertex.glsl
+++ src/game/shaders/vertex.glsl
@@ -28,8 +28,8 @@ void main() {
   const vec4 world_position = model_transform * vec4(position, 1.0);
 
   fragment.world_position = world_position.xyz;
-  fragment.world_normal = mat3(model_transform) * normal;
-  fragment.world_tangent = model_transform * tangent;
+  fragment.world_normal = mat3(model_transform) * normal; // model transform assumed to be an orthogonal matrix
+  fragment.world_tangent = vec4(mat3(model_transform) * tangent.xyz, tangent.w);
   fragment.texcoord_0 = texcoord_0;
 
   gl_Position = projection_transform * view_transform * world_position;
